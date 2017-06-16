9241872c05c22ad9f27460cf0eb82ce25792b534
Massinissa Saoudi <massinissa.saoudi@gmail.com>
2017-06-16 03:37:45 +0200

    Create Mogodb databse and connect to it

--- server.js
+++ server.js
@@ -3,9 +3,27 @@ console.log("Starting");
 var host = "127.0.0.1";
 var port = 1337
 
+var url = 'mongodb://127.0.0.1:27017/';
+var database = 'managment';
+var MongoClient = require('mongodb').MongoClient;
+var ObjectId = require('mongodb').ObjectID;
+var format = require('util').format;
+
+
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+
+var dbcon; 
+
+MongoClient.connect(url+database, function(err,db){
+	if(err){
+		throw err;
+	}
+	dbcon = db;
+	console.log("connected to the database");
+});
+
 
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
